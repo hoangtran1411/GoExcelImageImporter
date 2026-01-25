e9180ac458432c9ccb5b038a7bf7c5c9bcae0906
hoangtran1411 <chi3xitin2010@gmail.com>
2026-01-25 20:55:24 +0700

    chore(release): bump version to v2.1.0

--- updater.go
+++ updater.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Current version - update this when releasing new versions, or use ldflags
-var CurrentVersion = "v2.0.7"
+var CurrentVersion = "v2.1.0"
 
 // GitHub repository info
 const (
